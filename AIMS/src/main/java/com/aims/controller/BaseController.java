cc4bb8985a32eed0936e6e07935531546b908b78
Hoang <huyhoang2001037a1@gmail.com>
2025-12-24 23:03:48 +0700

    Delete AIMS directory

--- AIMS/src/main/java/com/aims/controller/BaseController.java
+++
@@ -1,5 +0,0 @@
-package com.aims.controller;
-
-public class BaseController {
-    
-}
--- AIMS/src/main/java/com/aims/utils/Configs.java
+++
@@ -1,6 +0,0 @@
-package com.aims.utils;
-
-public class Configs {
-    public static final double PERCENT_VAT = 10.0; // 10% VAT
-}
-
